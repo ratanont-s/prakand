3cc315ae91f1639c7c29cfba0c7818856c0ada20
rathanont <rathanont.s@gmail.com>
2021-03-10 22:44:12 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ $(function () {
   
     $('[data-toggle="offcanvas"]').on('click', function () {
       $('.offcanvas-collapse').toggleClass('open')
-      $('body').toggleClass('offcanvas-open')
     })
 })
 
