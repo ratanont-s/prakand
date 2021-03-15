68966db7d3bf75d5eb97d082b606e5b90b3ad629
odt-frontend <ratanont@owndays-tech.com>
2021-03-15 17:03:14 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,8 +22,6 @@ $.extend(true, $.fn.dataTable.defaults, {
   "dom": "<'row'<'col-12'tr>>" +
     "<'form-row'<'col-6 col-lg-3'l><'col-6 col-lg-6'p><'col-sm-12 col-lg-3 d-none d-lg-block'>>",
   drawCallback: function () {
-    $('.dataTables_paginate > .pagination').addClass(
-      'justify-content-end justify-content-lg-center');
     $('.dataTables_length').addClass('text-left');
   },
 });
